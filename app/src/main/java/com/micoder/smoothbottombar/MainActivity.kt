b79dc583c9c99c92e297af9663e2ca18163ec093
Micoder-dev <micoder.com@gmail.com>
2022-09-04 12:17:49 +0530

    Smooth Bottom Bar implemented with an exception

--- app/src/main/java/com/micoder/smoothbottombar/MainActivity.kt
+++ app/src/main/java/com/micoder/smoothbottombar/MainActivity.kt
@@ -2,10 +2,64 @@ package com.micoder.smoothbottombar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.micoder.smoothbottombar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding // view binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // view binding implementation
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.main_fragment)
+        setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
+
     }
+
+    // top menu bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.top_item1 -> {
+                showToast("item1 selected")
+            }
+            R.id.top_item2 -> {
+                showToast("item2 selected")
+            }
+            R.id.top_item3 -> {
+                showToast("item3 selected")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    // bottom nav bar with smooth nav bar
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
